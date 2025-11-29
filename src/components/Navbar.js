0dd9f7e4dbe456fe0bbec0cd3aa168928485394e
Ansh197 <77569137+Ansh197@users.noreply.github.com>
2025-11-29 15:15:22 +0530

    added handleLogin function for user login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Navbar.css'
 export default function Navbar(props) {
+
+  const handleLogin = () =>{
+    console.log('Login button clicked')
+  }
+
   return (
     <>
     <div className='navbar' style={props.mystyle} >
@@ -11,7 +16,7 @@ export default function Navbar(props) {
     </div>
     <button id='login' style={{color : `${props.mode==='Dark'?'black':'white'}`,
     backgroundColor :`${props.mode==='Dark'?'rgb(234, 232, 232)':'rgb(41, 41, 41)'}`}
-  }>
+  } onClick={handleLogin}>
       Login</button>
     </div>
     </>
